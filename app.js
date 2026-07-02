ea23c3859ad1db34a2d73106328d4bab01cd19e0
Srishthi <55523082+Srishthi@users.noreply.github.com>
2026-07-02 10:40:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -64,11 +64,36 @@ function generateTickets() {
         div.style.borderRadius = "8px";
         div.style.boxShadow = "0 2px 8px rgba(0,0,0,.1)";
 
-        div.innerHTML =
-            "<h3>WTS " +
-            String(startNumber + i).padStart(3, "0") +
-            "</h3>" +
-            "<p>Tambola ticket will appear here.</p>";
+       const ticket = generateTicket();
+
+let html =
+"<h3>WTS " +
+String(startNumber+i).padStart(3,"0") +
+"</h3>";
+
+html += "<table border='1' cellspacing='0' cellpadding='8'>";
+
+for(let r=0;r<3;r++){
+
+    html+="<tr>";
+
+    for(let c=0;c<9;c++){
+
+        html+="<td style='width:35px;height:35px;text-align:center;'>";
+
+        html+= ticket[r][c]===null ? "" : ticket[r][c];
+
+        html+="</td>";
+
+    }
+
+    html+="</tr>";
+
+}
+
+html+="</table>";
+
+div.innerHTML = html;
 
         preview.appendChild(div);
 
