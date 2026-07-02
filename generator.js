11de570fa088f366ec6d6334060f7b5c0b417310
Srishthi <55523082+Srishthi@users.noreply.github.com>
2026-07-02 10:36:13 +0530

    Update generator.js

--- generator.js
+++ generator.js
@@ -1,1 +1,21 @@
-console.log("Generator Loaded");
+/**
+ * ==========================================
+ * WTS Ticket Generator
+ * Version 0.0.4
+ * generator.js
+ * ==========================================
+ */
+
+function generateTicket() {
+
+    return [
+
+        [1,null,25,null,42,null,63,null,82],
+
+        [null,15,null,36,null,57,null,71,90],
+
+        [8,null,29,null,49,null,68,79,null]
+
+    ];
+
+}
